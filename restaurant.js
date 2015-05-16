8f9a7bf3748d9fc3d9e67158b3db5745866e18d6
Mark Paulson <markpaulson1@hotmail.com>
2015-05-16 18:37:21 -0400

    first commit

---
+++ restaurant.js
@@ -0,0 +1,78 @@
+
+$("button").click(function(){
+var selection = $(this).attr("id");
+console.log(selection);
+
+}
+);
+
+// if(selection.checked){
+
+// 	var pancakesButton = 
+// }
+
+// }
+
+
+var menu = {
+	"Pancakes": 6.95,
+	"Eggs Benedict": 9.95,
+	"Eggs Any Style": 5.95,
+	"Yougurt with Granola": 7.95,
+	"Add Bacon": 3.95,
+	"Coffee": 2.95,
+	"Orange Juice": 2.95,
+}
+
+
+
+
+
+
+
+ //  var log = document.getElementById('log');
+
+ //  function addLog(cssClass, message) {
+ //    var logEntry = document.createElement('li');
+ //    logEntry.innerHTML = message;
+ //    logEntry.className = cssClass;
+ //    log.appendChild(logEntry);
+ //    log.scrollTop = 999999;
+ //   }
+
+
+
+
+
+
+
+
+
+
+//var pancakesButton = document.getElementbyId('#pancakes');
+
+
+
+
+
+
+
+
+
+
+
+
+// var menu = function menu(food, price){
+// this.food = food;
+// this.price = price;
+// }
+
+// var pancakes = new menu ("Pancakes", 6.95);
+// var eggsBenedict = new menu("Eggs Benedict", 9.95);
+// var eggsStyle = new menu("Eggs Any Style", 5.95);
+// var yogurtGranola = new menu("Yougurt with Granola", 7.95);
+// var bacon = new menu("Add Bacon", 3.95);
+// var coffeeTea = new menu("Coffee", 2.95);
+// var orangeJuice = new menu("Orange Juice", 2.95);
+
+
