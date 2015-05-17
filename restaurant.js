e351cfea658c50dcfcee5f9dd9b0e3ec51a44975
Mark Paulson <markpaulson1@hotmail.com>
2015-05-17 18:18:12 -0400

    final updates

--- restaurant.js
+++ restaurant.js
@@ -5,35 +5,28 @@ function addToBill(){
   var itemPrice = menu[this.id];
   var total = 0;
 	total += itemPrice;
-
+// this isn't totaling correctly right now. I spent the weekend with 
+// my jquery in the wrong place and kept getting error messages so 
+// was very frustrated.
   $("#total").append(total);
-  
-
 }
 
 var menu = {
 	"pancakes": 6.95,
 	"eggsbenedict": 9.95,
-	"Eggs Any Style": 5.95,
-	"Yougurt with Granola": 7.95,
-	"Add Bacon": 3.95,
-	"Coffee": 2.95,
-	"Orange Juice": 2.95,
-}
+	"eggs": 5.95,
+	"yogurt": 7.95,
+	"bacon": 3.95,
+	"coffee": 2.95,
+	"juice": 2.95,
+};
 
-// var total = 0;
 
-// var calculateTotal = function(){}
-
-// total + menu[]
-
-// }
 
 $(document).ready(function(){
-  
   console.log('I am the JS file');  
   
-  // when I click .buton add to bill
+  // when I click .button add to bill
   $(".button").on('click', addToBill);
   
 });
@@ -42,6 +35,13 @@ $(document).ready(function(){
 $(document).ready(function(){
   console.log('I am the JS file');  
 });
+
+
+
+
+
+
+
 
 //CODE GRAVEYARD
 //I KEPT TRYING THINGS AND RUNNING INTO ERRORS AND IT NOT WORKING
@@ -83,12 +83,8 @@ $(document).ready(function(){
 
 //var pancakesButton = document.getElementbyId('#pancakes');
 
-
 /*
  window.onload = function() {}
-
-
-
 
  //  var log = document.getElementById('log');
 
@@ -112,22 +108,7 @@ var menu = {
 
 
 
-
-
-
-
-
 //var pancakesButton = document.getElementbyId('#pancakes');
-
-
-
-
-
-
-
-
-
-
 
 
 // var menu = function menu(food, price){
@@ -148,23 +129,5 @@ var menu = {
 
 
 
-
-
-
-
-
-
-// var menu = function menu(food, price){
-// this.food = food;
-// this.price = price;
-// }
-
-// var pancakes = new menu ("Pancakes", 6.95);
-// var eggsBenedict = new menu("Eggs Benedict", 9.95);
-// var eggsStyle = new menu("Eggs Any Style", 5.95);
-// var yogurtGranola = new menu("Yougurt with Granola", 7.95);
-// var bacon = new menu("Add Bacon", 3.95);
-// var coffeeTea = new menu("Coffee", 2.95);
-// var orangeJuice = new menu("Orange Juice", 2.95);
 
 
